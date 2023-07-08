09f6787eebb9bc8008b61d376a260c613f90f94e
Rasyid <rsyddn20@gmail.com>
2023-07-09 05:42:06 +0700

    initial commit

---
+++ libmath/src/main/java/com/example/libmath/calculate.kt
@@ -0,0 +1,8 @@
+package com.example.libmath
+
+class calculate {
+    fun time(first: Int, second: Int): Int = first * second
+    fun add(first: Int, second: Int): Int = first + second
+    fun subtract(first: Int, second: Int): Int = first - second
+    fun devide(first: Int, second: Int): Int = first / second
+}
